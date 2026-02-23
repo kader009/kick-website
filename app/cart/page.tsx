4fa3b16f6e640b0e44d6eed1206817fe1b3016c4
Md Abdul Kader Molla <kadermollarasel000@gmail.com>
2026-02-23 17:37:50 +0600

    cart design layout update

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -6,7 +6,7 @@ import Container from '@/src/components/ui/Container';
 import YouMayAlsoLike from '@/src/components/YouMayAlsoLike';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@/src/redux/store/store';
-import { removeFromCart, updateQuantity } from '@/src/redux/features/cartSlice';
+import { removeFromCart } from '@/src/redux/features/cartSlice';
 import toast from 'react-hot-toast';
 import { Open_Sans } from 'next/font/google';
 
@@ -31,15 +31,6 @@ export default function CartPage() {
     });
   };
 
-  const handleUpdateQty = (
-    id: number,
-    size: string,
-    color: string,
-    quantity: number,
-  ) => {
-    dispatch(updateQuantity({ id, size, color, quantity }));
-  };
-
   const subtotal = cartItems.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0,
@@ -55,16 +46,24 @@ export default function CartPage() {
           <h2 className="text-[32px] font-semibold text-[#232321] mb-2">
             Saving to celebrate
           </h2>
-          <p className={`${openSans.className} text-[#232321] text-[14px] w-[782px] font-semibold`}>
+          <p
+            className={`${openSans.className} text-[#232321] text-[14px] max-w-[782px] w-full font-semibold`}
+          >
             Enjoy up to 60% off thousands of styles during the End of Year sale
             - while supplies last. No code needed.
           </p>
           <div className="flex gap-2 mt-2 text-sm">
-            <Link href="#" className={`${openSans.className} font-semibold underline decoration-1 underline-offset-2 text-[16px]`}>
+            <Link
+              href="#"
+              className={`${openSans.className} font-semibold underline decoration-1 underline-offset-2 text-[16px]`}
+            >
               Join us
             </Link>
             <span>or</span>
-            <Link href="#" className={`${openSans.className} font-semibold underline decoration-1 underline-offset-2 text-[16px]`}>
+            <Link
+              href="#"
+              className={`${openSans.className} font-semibold underline decoration-1 underline-offset-2 text-[16px]`}
+            >
               Sign-in
             </Link>
           </div>
@@ -77,7 +76,9 @@ export default function CartPage() {
               <h1 className="text-[32px] font-semibold text-[#232321] mb-2 capitalize">
                 Your Bag
               </h1>
-              <p className={`${openSans.className} text-[#232321] text-[16px] mb-8 font-normal`}>
+              <p
+                className={`${openSans.className} text-[#232321] text-[16px] mb-8 font-normal`}
+              >
                 Items in your bag not reserved- check out now to make them
                 yours.
               </p>
@@ -99,50 +100,57 @@ export default function CartPage() {
                   {cartItems.map((item, index) => (
                     <div
                       key={`${item.id}-${item.size}-${item.color}`}
-                      className="flex flex-col md:flex-row gap-6 border-b border-gray-100 pb-8 last:border-0 last:pb-0"
+                      className="flex flex-row gap-4 md:gap-6 border-b border-gray-100 pb-8 last:border-0 last:pb-0"
                     >
                       {/* Product Image */}
-                      <div className="w-full md:w-[200px] aspect-square bg-[#ECEEF0] rounded-[24px] overflow-hidden shrink-0 flex items-center justify-center relative">
+                      <div className="w-[120px] md:w-[200px] aspect-square rounded-[24px] overflow-hidden shrink-0 flex items-center justify-center relative">
                         <Image
                           src={item.image}
                           alt={item.title}
                           unoptimized
-                          className="object-cover"
+                          className="w-full h-full object-contain p-2 rounded-[24px]"
                           width={207}
                           height={225}
                         />
                       </div>
 
                       {/* Info & Controls */}
-                      <div className="flex-1 flex flex-col justify-between">
-                        <div className="flex justify-between items-start">
-                          <div>
-                            <h3 className="text-xl font-semibold text-[#232321] uppercase leading-tight mb-2">
-                              {item.title}
-                            </h3>
-                            <p className={`${openSans.className} text-[#232321] font-semibold mb-2 line-clamp-1 text-[20px]`}>
-                              {item.description}
-                            </p>
-                            <p className={`${openSans.className} text-[#232321] font-semibold`}>
-                              {item.color}
-                            </p>
-                          </div>
-                          <span className="text-xl font-bold text-[#4A69E2]">
-                            ${item.price.toFixed(2)}
-                          </span>
-                        </div>
-
-                        <div className="flex flex-wrap gap-4 mt-6">
-                          {/* Size Selector Mock */}
-                          <div className="flex items-center gap-2">
-                            <span
-                              className={`${openSans.className} text-[#232321] font-semibold text-[20px]`}
-                            >
-                              Size {item.size}
+                      <div className="flex-1 flex flex-col justify-between py-1">
+                        <div>
+                          <div className="flex justify-between items-start">
+                            <div className="flex flex-col gap-1">
+                              <h3 className="text-[14px] md:text-xl font-bold text-[#232321] uppercase leading-tight">
+                                {item.title}
+                              </h3>
+                              <p
+                                className={`text-[12px] md:text-[18px] text-[#232321] font-medium leading-tight opacity-70 line-clamp-1`}
+                              >
+                                {item.description}
+                              </p>
+                              <p
+                                className={`text-[12px] md:text-[16px] text-[#232321] font-medium opacity-70`}
+                              >
+                                {item.color}
+                              </p>
+                              <div className="md:hidden mt-1">
+                                <span className="text-[16px] font-bold text-[#4A69E2]">
+                                  ${item.price.toFixed(2)}
+                                </span>
+                              </div>
+                            </div>
+                            <span className="hidden md:block text-xl font-bold text-[#4A69E2]">
+                              ${item.price.toFixed(2)}
                             </span>
-                            <button className="text-gray-600">
+                          </div>
+
+                          <div className="flex items-center gap-4 mt-2">
+                            <div className="flex items-center gap-1">
+                              <span
+                                className={`${openSans.className} text-[#232321] font-semibold text-[14px] md:text-[20px]`}
+                              >
+                                Size {item.size}
+                              </span>
                               <svg
-                                xmlns="http://www.w3.org/2000/svg"
                                 width="16"
                                 height="16"
                                 viewBox="0 0 24 24"
@@ -151,21 +159,18 @@ export default function CartPage() {
                                 strokeWidth="3"
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
+                                className="opacity-70"
                               >
                                 <path d="m6 9 6 6 6-6" />
                               </svg>
-                            </button>
-                          </div>
-                          {/* Quantity Selector Mock */}
-                          <div className="flex items-center gap-2">
-                            <span
-                              className={`${openSans.className} text-[#232321] font-semibold text-[20px]`}
-                            >
-                              Quantity {item.quantity}
-                            </span>
-                            <button className="text-gray-600">
+                            </div>
+                            <div className="flex items-center gap-1">
+                              <span
+                                className={`${openSans.className} text-[#232321] font-semibold text-[14px] md:text-[20px]`}
+                              >
+                                Quantity {item.quantity}
+                              </span>
                               <svg
-                                xmlns="http://www.w3.org/2000/svg"
                                 width="16"
                                 height="16"
                                 viewBox="0 0 24 24"
@@ -174,15 +179,16 @@ export default function CartPage() {
                                 strokeWidth="3"
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
+                                className="opacity-70"
                               >
                                 <path d="m6 9 6 6 6-6" />
                               </svg>
-                            </button>
+                            </div>
                           </div>
                         </div>
 
-                        <div className="flex gap-4 mt-8">
-                          <button className="text-gray-400 hover:text-black transition-colors">
+                        <div className="flex gap-4 mt-3">
+                          <button className="text-gray-900 transition-colors">
                             <svg
                               xmlns="http://www.w3.org/2000/svg"
                               width="24"
@@ -201,7 +207,7 @@ export default function CartPage() {
                             onClick={() =>
                               handleRemove(item.id, item.size, item.color)
                             }
-                            className="text-gray-400 hover:text-red-500 transition-colors"
+                            className="text-gray-900 transition-colors"
                           >
                             <svg
                               xmlns="http://www.w3.org/2000/svg"
@@ -213,7 +219,7 @@ export default function CartPage() {
                               strokeWidth="2"
                               strokeLinecap="round"
                               strokeLinejoin="round"
-                              className="p-[2px] border-2 border-current rounded-md"
+                              className="p-[2px] border-2 border-current rounded-md w-6 h-6"
                             >
                               <path d="M18 6 6 18" />
                               <path d="m6 6 12 12" />
@@ -229,7 +235,7 @@ export default function CartPage() {
           </div>
 
           {/* Right Side - Order Summary */}
-          <div className="w-full lg:w-[400px]">
+          <div className="w-full lg:w-[400px] bg-white p-6 rounded-[24px] lg:bg-transparent lg:p-0 lg:rounded-none">
             <h2 className="text-[32px] font-semibold text-[#232321] mb-[16px]">
               Order Summary
             </h2>
@@ -240,19 +246,27 @@ export default function CartPage() {
                   {cartItems.reduce((acc, obj) => acc + obj.quantity, 0)} ITEM
                   {cartItems.length !== 1 ? 'S' : ''}
                 </span>
-                <span className="font-semibold text-[20px]">${subtotal.toFixed(2)}</span>
+                <span className="font-semibold text-[20px]">
+                  ${subtotal.toFixed(2)}
+                </span>
               </div>
               <div className="flex justify-between items-center text-lg">
                 <span className="font-semibold text-[20px]">Delivery</span>
-                <span className="font-semibold text-[20px]">${delivery.toFixed(2)}</span>
+                <span className="font-semibold text-[20px]">
+                  ${delivery.toFixed(2)}
+                </span>
               </div>
               <div className="flex justify-between items-center">
                 <span className="font-semibold text-[20px]">Sales Tax</span>
                 <span className="font-semibold text-[20px]">-</span>
               </div>
               <div className="flex justify-between items-center text-xl pt-1">
-                <span className="font-semibold text-[24px] uppercase">Total</span>
-                <span className="font-semibold text-[24px]">${total.toFixed(2)}</span>
+                <span className="font-semibold text-[24px] uppercase">
+                  Total
+                </span>
+                <span className="font-semibold text-[24px]">
+                  ${total.toFixed(2)}
+                </span>
               </div>
             </div>
 
@@ -270,7 +284,7 @@ export default function CartPage() {
         </div>
       </Container>
 
-      <div className="mt-32">
+      <div className="mt-[24px] md:mt-[40px]">
         <YouMayAlsoLike />
       </div>
     </div>
